0312c5416aa7f85b78ae8d326d33702003c1e832
Olga1188 <olgasweet1188@rambler.ru>
2023-04-09 17:54:46 +0300

    Add file

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'olgasweet1188@rambler.ru'       # Ввести валидный email
+    valid_password = 'Radeon123'   # Ввести валидный пароль
+    valid_email_reg = 'Ввести валидный email'   # Ввести валидный email
+    invalid_email = 'olga118@rambler.com'
+    invalid_password = 'QWERTY12345'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Ол'
+    last_name = 'Ма'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'м'
+    latin_generate_string = 'QWerty'
+    chinese_chars = '形聲字象形字形声字/'
+    special_chars = '()_+=/{}!@#$%^&*'
+    numbers = 777
+    empty = ''
+    passw1 = 'QwE12345'
+    passw2 = 'Z56vBn09Dswa9874'
+    passw3 = 'W321Ujb30aSwsX1987654'
